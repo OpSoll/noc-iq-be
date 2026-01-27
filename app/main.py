e198043ba913b6a9e94e475a4daca1fb4927ce67
mxllv <semilorefadumila@gmail.com>
2026-01-27 11:04:40 +0100

    fix: no cors error

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from app.api.v1.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="NOCIQ API",
     version="1.0.0",
     description="NOCIQ Backend API"
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Health check
 @app.get("/health")
